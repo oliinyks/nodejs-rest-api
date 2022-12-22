5cb6c7341b0255b8170811d00a9a0fea1e746666
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-12-22 16:13:15 +0200

    add

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,16 +1,18 @@
 const { isValidObjectId } = require("mongoose");
-const { handleSchemaValidationErrors } = require('../helpers/handlerSchemaValidation');
+const {
+  handleSchemaValidationErrors,
+} = require("../helpers/handlerSchemaValidation");
 
 const isValidId = (req, _, next) => {
-	const { contactId } = req.params;
-	if (!isValidObjectId(contactId)) {
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
     const error = handleSchemaValidationErrors(
       400,
       `${contactId} is not corrent id format`
     );
     next(error);
   }
-	next()
-}
+  next();
+};
 
 module.exports = isValidId;
