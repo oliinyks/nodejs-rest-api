8718da058533f17c8c3f95ce36fede72e051d478
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-12-28 13:32:39 +0200

    add contact favorite

--- src/controllers/contacts/getContacts.js
+++ src/controllers/contacts/getContacts.js
@@ -1,20 +1,15 @@
-const Contact = require("../../models/contact");
+const {Contact} = require("../../models/contact");
 
 const getContacts = async (req, res) => {
   const { _id } = req.user;
   const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const match = {};
-  if (favorite) {
-    match.favorite = favorite === "true";
-  }
-
   const contacts = await Contact.find(
-    { owner: _id },
+    favorite ? { owner: _id, favorite } : { owner: _id },
     "-createdAt, -updatedAt",
     { skip, limit: Number(limit) }
-  ).populate("owner", "_id email", match);
+  ).populate("owner", "_id email");
 
   res.json({
     status: "success",
