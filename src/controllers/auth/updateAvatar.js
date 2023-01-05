7dc4c4d14507f0cabdecc1b8d231cad7c25e255c
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2023-01-05 11:51:44 +0200

    Update updateAvatar.js

--- src/controllers/auth/updateAvatar.js
+++ src/controllers/auth/updateAvatar.js
@@ -9,20 +9,19 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
   const imageName = `${id}_${originalname}`;
-	try {
-		
-		const resultUpload = path.join(avatarsDir, imageName);
-		await fs.rename(tempUpload, resultUpload);
+  try {
+    const resultUpload = path.join(avatarsDir, imageName);
+    await fs.rename(tempUpload, resultUpload);
 
-		Jimp.read(resultUpload)
+    Jimp.read(resultUpload)
       .then((img) => {
         return img.resize(250, 250).write(resultUpload);
       })
       .catch((error) => {
         throw error;
       });
-		
-		const avatarURL = path.join("public", "avatars", imageName);
+
+    const avatarURL = path.join("public", "avatars", imageName);
 
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
